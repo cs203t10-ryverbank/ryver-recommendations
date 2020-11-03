3f6f21dbec21afe09d8b57cda2832f6dc8fe9e12
Bryan Lee <bryanmylee@gmail.com>
2020-11-04 05:46:41 +0800

    Get customer content

--- src/app.py
+++ src/app.py
@@ -1,4 +1,6 @@
 import flask
+from flask import request
+import json
 import py_eureka_client.eureka_client as eureka_client
 
 eureka_client.init(eureka_server="http://localhost:8761/eureka",
@@ -14,8 +16,24 @@ except Exception:
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 
+
 @app.route("/", methods=["GET"])
 def index():
     return "ryver-recommendations service"
+
+
+@app.route("/<int:customer_id>", methods=["GET"])
+def get_recommendations(customer_id):
+    try:
+        auth = request.headers["Authorization"]
+        if auth == None or not auth.startswith("Bearer"):
+            return "Bearer token required for recommendations service"
+        res = eureka_client.do_service("ryver-cms", "/contents", headers=request.headers)
+        body = json.loads(res)
+        print("cms content:", body)
+        return f"Customer {customer_id} asking for recommendations"
+    except Exception:
+        return "Getting recommendations failed", 500
+
 
 app.run(host="0.0.0.0", port=8084)
